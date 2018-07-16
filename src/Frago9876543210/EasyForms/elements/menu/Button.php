3e42c91e560e1edee86f84817841afa78312b41e
alex <al.volf.av@gmail.com>
2018-07-16 21:27:22 +0300

    Added type parameter for buttons

--- src/Frago9876543210/EasyForms/elements/menu/Button.php
+++ src/Frago9876543210/EasyForms/elements/menu/Button.php
@@ -8,17 +8,24 @@ namespace Frago9876543210\EasyForms\elements\menu;
 use Frago9876543210\EasyForms\elements\Element;
 
 class Button extends Element{
+	public const TYPE_URL = "url";
+	public const TYPE_PATH = "path";
+
 	/** @var null|string */
 	protected $image;
+	/** @var string */
+	protected $type;
 
 	/**
 	 * Button constructor.
 	 * @param string      $text
 	 * @param null|string $image
+	 * @param string      $type
 	 */
-	public function __construct(string $text, ?string $image = null){
+	public function __construct(string $text, ?string $image = null, string $type = self::TYPE_URL){
 		parent::__construct($text);
 		$this->image = $image;
+		$this->type = $type;
 	}
 
 	/**
@@ -35,7 +42,7 @@ class Button extends Element{
 		$data = ["text" => $this->text];
 		if($this->hasImage()){
 			$data["image"] = [
-				"type" => "url",
+				"type" => $this->type,
 				"data" => $this->image
 			];
 		}
